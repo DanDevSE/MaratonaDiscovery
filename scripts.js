abc41723c626b63f3b70e9cadbf7b97520f3362b
Dan <danilobomfim.si@gmail.com>
2021-05-07 18:25:09 -0300

    update

--- scripts.js
+++ scripts.js
@@ -33,22 +33,40 @@ const transactions = [
 const Transaction = {};
 
 const DOM = {
-  addTransaction(transaction, index){
-    const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction()
-  },  
-  innerHTMLTransaction() {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
     const html = `    
       <tr>
-        <td class="description">Luz</td>
-        <td class="expense">- R$ 500,00</td>
-        <td class="date">19/06/2021</td>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" alt="Remover transação" />
         </td>
       </tr>
-      `
+      `;
 
-      return html
+    return html;
   },
 };
+
+const Utils = {
+    formatCurrency(value){
+ const signal = Number(value) < 0 ? "-": ""
+    }
+}
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction);
+});
