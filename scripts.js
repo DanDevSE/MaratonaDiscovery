bffedd4a49efb748b39f47fad5bd9aab13ef32ab
Dan <danilobomfim.si@gmail.com>
2021-05-12 20:03:27 -0300

    add

--- scripts.js
+++ scripts.js
@@ -30,7 +30,17 @@ const transactions = [
   },
 ];
 
-const Transaction = {};
+const Transaction = {
+  incomes() {
+    return "cheguei";
+  },
+  expenses() {
+    return "aqui";
+  },
+  total() {
+    return "discovery";
+  },
+};
 
 const DOM = {
   transactionsContainer: document.querySelector("#data-table tbody"),
@@ -44,12 +54,12 @@ const DOM = {
   innerHTMLTransaction(transaction) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
-    const amount = Utils.formatCurrency(transaction.amount)
+    const amount = Utils.formatCurrency(transaction.amount);
 
     const html = `    
       <tr>
         <td class="description">${transaction.description}</td>
-        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" alt="Remover transação" />
@@ -59,14 +69,35 @@ const DOM = {
 
     return html;
   },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Transaction.incomes();
+
+    document.getElementById("expenseDisplay").innerHTML = Transaction.expenses();
+
+    document.getElementById("totalDisplay").innerHTML = Transaction.total()
+  },
 };
 
 const Utils = {
-    formatCurrency(value){
- const signal = Number(value) < 0 ? "-": ""
-    }
-}
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
 
 transactions.forEach(function (transaction) {
   DOM.addTransaction(transaction);
 });
+
+DOM.updateBalance();
